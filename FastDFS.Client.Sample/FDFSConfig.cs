27122f1488266b84e5c0b8dad15d31057e64a236
wzh0717 <w-z-h06@163.com>
2020-04-29 11:42:42 +0800

    init

---
+++ FastDFS.Client.Sample/FDFSConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace FastDFS.Client.Sample
+{
+    public class FDFSConfig
+    {
+        public String TackerIp { get; set; }
+
+        public String TackerPort { get; set; }
+
+        public String StorageGroup { get; set; }
+
+        public String StorageServerLink { get; set; }
+    }
+}
---
+++ FastDFS.Client/README.md
@@ -0,0 +1,3 @@
+﻿#### 基于asp.net core 3.1 FastDFS客户端
+
+* 代码源于https://github.com/caozhiyuan/FastDFSNetCore.git
